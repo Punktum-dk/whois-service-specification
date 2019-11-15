845c792538664aa85b13e116e8d94c010e9ecf5f
jonasbn <jonasbn@dk-hostmaster.dk>
2019-11-15 10:24:23 +0100

    Improved output from GitHub Markdownlint action

--- github-action-markdownlint/entrypoint.sh
+++ github-action-markdownlint/entrypoint.sh
@@ -4,4 +4,9 @@ echo ""
 echo "Using Markdownlint on all Markdown files"
 echo "----------------------------------------"
 
-find . -name "*.md" | xargs markdownlint
+find . -name "*.md" | xargs -t markdownlint
+
+EXITCODE=$?
+
+test $EXITCODE -eq 0 && echo "Everything looks good" || echo "Markdown does not comply with Markdownlint configuration";
+exit $EXITCODE
